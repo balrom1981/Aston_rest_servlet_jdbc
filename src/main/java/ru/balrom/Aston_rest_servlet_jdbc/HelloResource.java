a50ffa760fb4f6408905443dc48f67c24cf0b807
balrom1981 <balrom@bk.ru>
2025-02-20 14:32:26 +0300

    Project created in the main

---
+++ src/main/java/ru/balrom/Aston_rest_servlet_jdbc/HelloResource.java
@@ -0,0 +1,14 @@
+package ru.balrom.Aston_rest_servlet_jdbc;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
